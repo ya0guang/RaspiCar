4ff2224dec9474f9548b31ca41da20caef9e3a16
zxcvfd13502 <30365411+zxcvfd13502@users.noreply.github.com>
2018-01-07 16:08:02 +0800

    improved the PID class

--- PIDController.py
+++ PIDController.py
@@ -1,51 +1,71 @@
 import time
 
-class PIDController:
-    def __init__(self, P=0.0, I=0.0, D=0.0, sample=0.0):
-        self.last_error = 0
+class PIDController():
+	
+    def __init__(self, P=0.0, I=0.0, D=0.0, sample=1.0):
+
         # set PID parameters
         self.Kp = P
         self.Ki = I
         self.Kd = D
-        self.sample_time = sample
+        self.sampleTime = sample
+
         # initialize time
-        self.current_time = time.time()
-        self.last_time = self.current_time
+        self.currentTime = time.time()
+        self.lastTime = self.currentTime
         self.clear()
 		
     def clear(self):
         self.PTerm = 0.0
         self.ITerm = 0.0
         self.DTerm = 0.0
-        self.last_error = 0.0
+        self.lastError = 0.0
         self.output = 0.0
+        self.lastoutput = 0.0
 
-    def update(self, positionL, positionR):
-        error = positionL - positionR
+    def update(self, setpoint, feedback):
+        error = setpoint - feedback
 		
-        self.current_time = time.time()
-        delta_time = self.current_time - self.last_time
-        delta_error = error - self.last_error
+        self.currentTime = time.time()
+        deltaTime = self.currentTime - self.lastTime
+        deltaError = error - self.lastError
 		# Calculate PID terms
-        if (delta_time >= self.sample_time):
+        if (delta_Time >= self.sampleTime):#how to get delta_time?????
             self.PTerm = error
-			self.ITerm += error * delta_time
-            self.DTerm = delta_error
+            self.ITerm += error * deltaTime
+            self.DTerm = deltaError
 
             # Remember last time and last error for next calculation
-            self.last_time = self.current_time
-            self.last_error = error
+            self.lastTime = self.currentTime
+            self.lastError = error
 
-            self.output = (self.Kp * self.PTerm) + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+            self.output = (self.Kp * self.PTerm) + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)+self.lastoutput
+            self.lastoutput=self.output
 
-    def setKp(self, proportional_gain):
-        self.Kp = proportional_gain
-
-    def setKi(self, integral_gain):
-        self.Ki = integral_gain
-
-    def setKd(self, derivative_gain):
-        self.Kd = derivative_gain
-
-    def setSampleTime(self, sample_time):
-        self.sample_time = sample_time
+class EngineController():
+    # Feedback impluses received per sample period
+    #speed = (1350/4, 1350/3, 1350/2, 1350)
+	
+    def __init__(self, level):
+        self.controllerMain = PIDController()
+        #self.controllerSub = PIDController()
+        # Speed level
+        self.speed = 0
+        #self.speedR=0
+		
+    # def configure(self, num, P=0.0, I=0.0, D=0.0, sample=1.0):
+    #     # Main controller
+    #     self.controllerMain = PIDController(P=0.0, I=0.0, D=0.0, sample=1.0)
+    #     # if num == 1:
+    #         # self.controllerMain = PIDController(P=0.0, I=0.0, D=0.0, sample=1.0)
+    #     # Sub controller
+    #     # if num == 2:
+    #         # self.controllerSub = PIDController(P=0.0, I=0.0, D=0.0, sample=1.0)
+	
+    def update(self, speed, sepdet):
+        self.controllerMain.update(speed, sepdet)
+         # Take the left engine as main engine
+        # self.controllerSub.update(speedR, sepdetR)
+		
+    def setSpeed(self, level):
+         self.level = level
